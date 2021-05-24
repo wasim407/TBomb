4c97015052b50ac11c4cbe67c28fbdf8970f586d
wasim407 <84659258+wasim407@users.noreply.github.com>
2021-05-24 15:13:31 +0530

    Update TBomb.sh

--- TBomb.sh
+++ TBomb.sh
@@ -107,9 +107,9 @@ do
     banner
     echo -e "\e[4;31m Please Read Instruction Carefully !!! \e[0m"
     echo " "
-    echo "Press 1 To  Start SMS  Bomber "
-    echo "Press 2 To  Start CALL Bomber "
-    echo "Press 3 To  Start MAIL Bomber (Not Yet Available)"
+    echo "Press 1 To  start SMS bombing 🎆 "
+    echo "Press 2 To  Start CALL Bomber🎆 "
+    echo "Press 3 To  Start MAIL Bomber🎆 (Not Yet Available)"
     echo "Press 4 To  Update (Works On Linux And Linux Emulators) "
     echo "Press 5 To  Exit "
     read ch
